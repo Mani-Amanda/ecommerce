a74cf02ce539f54461670de8cdf316b3d354a075
Mani-Amanda <135225894+Mani-Amanda@users.noreply.github.com>
2024-06-06 18:36:51 +0530

    complete login

---
+++ lib/features/authentication/screens/login/login.dart
@@ -0,0 +1,123 @@
+import 'package:ecommerce/common/widgets/login_signup/form_divider.dart';
+import 'package:ecommerce/common/widgets/login_signup/social_buttons.dart';
+import 'package:ecommerce/common/widgets/styles/spacing_styles.dart';
+import 'package:ecommerce/utils/constants/colors.dart';
+import 'package:ecommerce/utils/constants/image_strings.dart';
+import 'package:ecommerce/utils/constants/sizes.dart';
+import 'package:ecommerce/utils/constants/text_strings.dart';
+import 'package:ecommerce/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelpFunctions.isDarkMode(context);
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: TSpacingStyle.paddingWithAppBarHeight,
+          child: Column(
+            children: [
+              ///Logo,Title & Sub-Title
+
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image(
+                    height: 150,
+                    image: AssetImage(
+                        dark ? TImages.lightApplogo : TImages.darkAppLogo),
+                  ),
+                  Text(TText.loginTitle,
+                      style: Theme.of(context).textTheme.headlineMedium),
+                  const SizedBox(
+                    height: TSizes.sm,
+                  ),
+                  Text(TText.loginSubTitle,
+                      style: Theme.of(context).textTheme.bodyMedium),
+                ],
+              ),
+
+              ///Form
+              Form(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: TSizes.spaceBtwSections),
+                  child: Column(
+                    children: [
+                      ///Email
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          prefixIcon: Icon(Iconsax.direct_right),
+                          labelText: TText.email,
+                        ),
+                      ),
+                      const SizedBox(height: TSizes.spaceBtwInputFields),
+
+                      ///Password
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          prefixIcon: Icon(Iconsax.password_check),
+                          labelText: TText.password,
+                          suffixIcon: Icon(Iconsax.eye_slash),
+                        ),
+                      ),
+                      const SizedBox(height: TSizes.spaceBtwInputFields / 2),
+
+                      ///Remeber Me & Forget Password
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          ///Remember me
+                          Row(
+                            children: [
+                              Checkbox(value: true, onChanged: (value) {}),
+                              const Text(TText.rememberMe),
+                            ],
+                          ),
+
+                          ///Forget password
+                          TextButton(
+                              onPressed: () {},
+                              child: const Text(TText.forgetPassword)),
+                        ],
+                      ),
+                      const SizedBox(height: TSizes.spaceBtwSections),
+
+                      ///sign in button
+                      SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                              onPressed: () {},
+                              child: const Text(TText.signIn))),
+                      const SizedBox(height: TSizes.spaceBtwItem),
+
+                      ///Create account Button
+                      SizedBox(
+                          width: double.infinity,
+                          child: OutlinedButton(
+                              onPressed: () {},
+                              child: const Text(TText.creatAccount))),
+                    ],
+                  ),
+                ),
+              ),
+
+              ///Divider
+              TFormDivider(dividerText: TText.orSignInWith.capitalize!),
+              const SizedBox(height: TSizes.spaceBtwSections),
+
+              ///footer
+              TSocialButtons(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
