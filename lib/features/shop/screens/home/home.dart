bcf7e9ae4b55184c8122fbdb88296c94bbf3427d
Mani-Amanda <135225894+Mani-Amanda@users.noreply.github.com>
2024-06-26 22:28:21 +0530

    remove unused lib

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,11 +1,5 @@
-import 'package:ecommerce/common/widgets/custom_shapes/Containers/circular_container.dart';
 import 'package:ecommerce/common/widgets/custom_shapes/Containers/primary_header_container.dart';
-import 'package:ecommerce/common/widgets/custom_shapes/curved_edges/curved_edges.dart';
-import 'package:ecommerce/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
-import 'package:ecommerce/utils/constants/colors.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
