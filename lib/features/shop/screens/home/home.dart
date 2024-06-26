8411e6e6620edf2be2f0b5daba0347c56e5be8be
Mani-Amanda <135225894+Mani-Amanda@users.noreply.github.com>
2024-06-26 22:26:51 +0530

    complete clip path

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,10 +1,25 @@
+import 'package:ecommerce/common/widgets/custom_shapes/Containers/circular_container.dart';
+import 'package:ecommerce/common/widgets/custom_shapes/Containers/primary_header_container.dart';
+import 'package:ecommerce/common/widgets/custom_shapes/curved_edges/curved_edges.dart';
+import 'package:ecommerce/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
+import 'package:ecommerce/utils/constants/colors.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+        body: SingleChildScrollView(
+            child: Column(
+      children: [
+        TPrimaryHeaderContainer(
+          child: Container(),
+        ),
+      ],
+    )));
   }
 }
